fc0975c5919d34ccc72f3bb0cccd142349872ff8
Folusho <folusho@lanthanidesoft.com>
2017-03-11 02:22:53 +0100

    Imported findMinMax.

--- spec/tests/findMinMaxSpec.js
+++ spec/tests/findMinMaxSpec.js
@@ -2,6 +2,9 @@
 
 (function() {
   describe('Min-Max Numbers in a List: ', function() {
+    var myApp = require("../../app/findMinMax.js");
+    var findMinMax = myApp.findMinMax;
+
   	describe('Return the min and max number in the list in a new list follows `[min, max]`', function() {
       it('should return [1,4] for [1, 2, 3 , 4]', function() {
         expect(findMinMax([1, 2, 3, 4])).toEqual([1, 4]);
