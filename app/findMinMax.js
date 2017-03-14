9e26f3cdb2e2f8d678454e7d3b83ecedb65e03ed
Folusho <folusho@lanthanidesoft.com>
2017-03-14 07:20:58 +0100

    add code to pass all tests.

--- app/findMinMax.js
+++ app/findMinMax.js
@@ -6,6 +6,14 @@ module.exports = {
     if (!(arrayOfNumbers instanceof Array)) {
       return 'Argument not an an array';
     }
+    if (arrayOfNumbers.length == 0) {
+      return 'Array argument must not be empty';
+    }
+
+    // Handle trivial case.
+    if (arrayOfNumbers.length == 1) {
+      return [arrayOfNumbers[0]];
+    }
 
     var min = arrayOfNumbers[0];
     var max = arrayOfNumbers[0];
